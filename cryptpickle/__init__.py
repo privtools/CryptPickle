77ce3784fe1087edc133a541bed9b600913771aa
privacy-dad <privacy-dad@protonmail.com>
2022-08-17 23:04:44 +0200

    linted code

--- cryptpickle/__init__.py
+++ cryptpickle/__init__.py
@@ -1,11 +1,12 @@
 """
-Library for encrypted python object serialization
+Library for encrypted python object serialization.
 
 CryptPickle allows you to easily encrypt python objects into a file and decrypt, regardless of their content. 
 It may be any python object, including for example a Pandas DataFrame.
 """
 
-__version__ = "0.2"
+
+__version__ = "0.3"
 
 
 import pyzipper
@@ -18,6 +19,7 @@ NO_PASSWD = None
 
 
 def obj_to_encrypted(obj, password=NO_PASSWD, path=DATA_PATH):
+    """Encrypt object data to file"""
     buf = pickle.dumps(obj)
     pwd = password
     with pyzipper.AESZipFile(path,
@@ -29,6 +31,7 @@ def obj_to_encrypted(obj, password=NO_PASSWD, path=DATA_PATH):
 
 
 def obj_from_encrypted(password=NO_PASSWD, path=DATA_PATH):
+    """Decrypt object data to file"""
     pwd = password
     with pyzipper.AESZipFile(path) as zf:
         zf.setpassword(pwd.encode('utf-8'))
